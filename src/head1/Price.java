77c7a2e780bcfb73c9367a3eaf80c10f0a420848
zheka <zheka0783@gmail.com>
2016-12-08 19:21:02 +0200

    Refactoring 8
    changed getFrequentRenterPoints() method

--- src/head1/Price.java
+++ src/head1/Price.java
@@ -6,10 +6,6 @@ public abstract class Price {
 	abstract double getCharge(int daysRented);
 	
 	int getFrequentRenterPoints(int daysRented) {
-		if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
-			return 2;
-		} else {
-			return 1;
-		}
+		return 1;
 	} 
 }
