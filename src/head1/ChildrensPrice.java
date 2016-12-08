44548569c6fc82908a5f9a343b5b86aa7e611973
zheka <zheka0783@gmail.com>
2016-12-08 19:15:55 +0200

    Refactoring 7
    Redefine getCharge() method to abstract

--- src/head1/ChildrensPrice.java
+++ src/head1/ChildrensPrice.java
@@ -1,7 +1,17 @@
 package head1;
 
 public class ChildrensPrice extends Price {
+	@Override
 	int getPriceCode() {
 		return Movie.CHILDRENS;
+	}
+	
+	@Override
+	double getCharge(int daysRented) {
+		double result = 1.5;
+		if (daysRented > 3) {
+			result += (daysRented - 3) * 1.5;
+		}
+		return result;
 	}
 }
--- src/head1/NewReleasePrice.java
+++ src/head1/NewReleasePrice.java
@@ -1,7 +1,13 @@
 package head1;
 
 public class NewReleasePrice extends Price {
+	@Override
 	int getPriceCode() {
 		return Movie.NEW_RELEASE;
+	}
+
+	@Override
+	double getCharge(int daysRented) {
+		return daysRented * 3;
 	}
 }
