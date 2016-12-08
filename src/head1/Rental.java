b4f8a24103d683d7170a6c9ee61982b4fdf4619f
zheka <zheka0783@gmail.com>
2016-12-08 15:31:19 +0200

    Refactoring 1
    Moved getCharge() method from Rental to Movie class

--- src/head1/Rental.java
+++ src/head1/Rental.java
@@ -1,7 +1,7 @@
 package head1;
 
 public class Rental {
-	private Movie _movie;
+	Movie _movie;
 	private int _daysRented;
 	
 	public Rental(Movie movie, int daysRented) {
@@ -16,27 +16,9 @@ public class Rental {
 	public int getDaysRented() {
 		return _daysRented;
 	}
-
-	double getCharge() {
-		double result = 0.0;
-		switch (getMovie().getPriceCode()) {
-			case Movie.REGULAR :
-				result += 2;
-				if (getDaysRented() > 2) {
-					result += (getDaysRented() - 2) * 1.5;
-				}
-				break;
-			case Movie.NEW_RELEASE :
-				result += getDaysRented() * 3;
-				break;
-			case Movie.CHILDRENS :
-				result += 1.5;
-				if (getDaysRented() > 3) {
-					result += (getDaysRented() - 3) * 1.5;
-				}
-				break;
-		}
-		return result;
+	
+	public double getCharge() {
+		return _movie.getCharge(_daysRented);
 	}
 
 	int getFrequentRenterPoints() {
