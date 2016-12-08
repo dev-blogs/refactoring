35e91b2e94aa05c34220cef9eae30c48b10361d7
zheka <zheka0783@gmail.com>
2016-12-08 18:48:58 +0200

    Refactoring
    Changed App class

--- src/head1/App.java
+++ src/head1/App.java
@@ -5,11 +5,11 @@ import java.util.TimeZone;
 
 public class App {
 	public static void main(String [] args) {
-		Movie movie1 = new Movie("movie1", 1);
-		Movie movie2 = new Movie("movie2", 2);
-		Movie movie3 = new Movie("movie3", 3);
-		Movie movie4 = new Movie("movie4", 4);
-		Movie movie5 = new Movie("movie5", 5);
+		Movie movie1 = new Movie("movie1", Movie.REGULAR);
+		Movie movie2 = new Movie("movie2", Movie.NEW_RELEASE);
+		Movie movie3 = new Movie("movie3", Movie.CHILDRENS);
+		Movie movie4 = new Movie("movie4", Movie.REGULAR);
+		Movie movie5 = new Movie("movie5", Movie.NEW_RELEASE);
 		
 		Rental rental1 = new Rental(movie1, 5);
 		Rental rental2 = new Rental(movie2, 3);
