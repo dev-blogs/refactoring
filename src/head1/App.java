4fa4c7bef515ccbda4a0ba9c6882eaa60d34ffea
zheka <zheka0783@gmail.com>
2016-12-14 18:55:44 +0200

    Refactoring in App class
    Added some additional movies

--- src/head1/App.java
+++ src/head1/App.java
@@ -1,23 +1,22 @@
 package head1;
 
-import java.util.Date;
-import java.util.TimeZone;
-
 public class App {
 	public static void main(String [] args) {
 		Movie movie1 = new Movie("movie1", 1);
 		Movie movie2 = new Movie("movie2", 2);
-		Movie movie3 = new Movie("movie3", 3);
-		Movie movie4 = new Movie("movie4", 4);
-		Movie movie5 = new Movie("movie5", 5);
+		Movie movie3 = new Movie("movie3", 1);
+		Movie movie4 = new Movie("movie3", 1);
 		
 		Rental rental1 = new Rental(movie1, 5);
 		Rental rental2 = new Rental(movie2, 3);
 		Rental rental3 = new Rental(movie3, 4);
+		Rental rental4 = new Rental(movie4, 1);
 		
 		Customer customer = new Customer("customer");
 		customer.addRental(rental1);
 		customer.addRental(rental2);
+		customer.addRental(rental3);
+		customer.addRental(rental4);
 		
 		System.out.println(customer.statement());
 	}
