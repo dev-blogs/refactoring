8370bfa17641fa6c30ebc7905c476133f0ce6cff
zheka <zheka0783@gmail.com>
2016-12-15 16:36:21 +0200

    Refactoring. Changed field code price simple variable with Price class
    _priceCode no longer set in Movie object. Instead of that simple field
    _priceCode was changed by Price class. What class of Price will be put
    into field Price depends on _priceCode variable which pass into
    setPriceCode method from constructor

--- src/head1/Movie.java
+++ src/head1/Movie.java
@@ -6,7 +6,7 @@ public class Movie {
 	public static final int NEW_RELEASE = 1;
 	
 	private String _title;
-	private int _priceCode;
+	private Price _price;
 	
 	public Movie(String title, int priceCode) {
 		_title = title;
@@ -22,11 +22,23 @@ public class Movie {
 	}
 
 	public int getPriceCode() {
-		return _priceCode;
+		return _price.getPriceCode();
 	}
 
 	public void setPriceCode(int arg) {
-		this._priceCode = arg;
+		switch (arg) {
+			case REGULAR :
+				_price = new RegularPrice();
+				break;
+			case CHILDRENS :
+				_price = new ChildrensPrice();
+				break;
+			case NEW_RELEASE :
+				_price = new NewReleasePrice();
+				break;
+			default :
+				throw new IllegalStateException("Incorrect Price Code");
+		}
 	}
 
 	double getCharge(int daysRented) {
