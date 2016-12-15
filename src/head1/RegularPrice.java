6febe45282954bded02736fa176fe1321d60ec7a
zheka <zheka0783@gmail.com>
2016-12-15 16:52:22 +0200

    Refactoring. Replace conditional with Polymorphism
    Took each switch's branch logic which calculates sum of concrete type of
    movie and moved it into subclass for corresponded movie type

--- src/head1/RegularPrice.java
+++ src/head1/RegularPrice.java
@@ -5,4 +5,13 @@ public class RegularPrice extends Price {
 	int getPriceCode() {
 		return Movie.REGULAR;
 	}
+
+	@Override
+	double getCharge(int daysRented) {
+		double result = 2.0;
+		if (daysRented > 2) {
+			result += (daysRented - 2) * 1.5;
+		}
+		return result;
+	}
 }
