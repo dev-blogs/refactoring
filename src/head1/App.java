f693e5f3c84db2f750d4839632c9f1290597d65e
zheka <zheka0783@gmail.com>
2016-12-15 15:32:07 +0200

    Refactoring
    Changed digit's priceCode to named constants in App class

--- src/head1/App.java
+++ src/head1/App.java
@@ -2,10 +2,10 @@ package head1;
 
 public class App {
 	public static void main(String [] args) {
-		Movie movie1 = new Movie("movie1", 1);
-		Movie movie2 = new Movie("movie2", 2);
-		Movie movie3 = new Movie("movie3", 0);
-		Movie movie4 = new Movie("movie4", 1);
+		Movie movie1 = new Movie("movie1", Movie.NEW_RELEASE);
+		Movie movie2 = new Movie("movie2", Movie.CHILDRENS);
+		Movie movie3 = new Movie("movie3", Movie.REGULAR);
+		Movie movie4 = new Movie("movie4", Movie.NEW_RELEASE);
 		
 		Rental rental1 = new Rental(movie1, 5);
 		Rental rental2 = new Rental(movie2, 3);
